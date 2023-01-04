9c6234a59098fc34aa9ec4ec02c52ee69ddc4f16
Christian <cjfuentes27@ieszaidinvergeles.org>
2023-01-04 15:41:33 +0100

    Testimonials ps slider done

---
+++ js/slider.js
@@ -0,0 +1,9 @@
+new Glider(document.querySelector('.testimonials-slider'),{
+    slidesToShow: 1,
+    dots: '.testimonials-dots',
+    draggable: false,
+    arrows: {
+        prev: '.arrow-box-left',
+        next: '.arrow-box-right'
+    }
+});
