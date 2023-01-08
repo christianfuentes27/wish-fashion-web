4bb07cfa13b60edd5d1436aec5ebde82e2219c69
Christian <cjfuentes27@ieszaidinvergeles.org>
2023-01-08 16:45:07 +0100

    Testimonials responsive

--- js/slider.js
+++ js/slider.js
@@ -1,7 +1,8 @@
 new Glider(document.querySelector('.testimonials-slider'),{
     slidesToShow: 1,
     dots: '.testimonials-dots',
-    draggable: false,
+    scrollLock: true,
+    draggable: true,
     arrows: {
         prev: '.arrow-box-left',
         next: '.arrow-box-right'
